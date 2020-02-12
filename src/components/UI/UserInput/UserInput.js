f2cefdac9bef8437abaf221a69b4a7cbf51a8b6d
alekgomes <lucas.maistro@gmail.com>
2020-02-12 09:46:07 -0300

    clean form after submit

--- src/components/UI/UserInput/UserInput.js
+++ src/components/UI/UserInput/UserInput.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserInput extends Component {
   render() {
     return (
-      <input placeholder={this.props.placeholder} onChange={this.props.handleInput} name={this.props.name} type={this.props.type} minLength={this.props.minLength}/>
-    )
+      <input
+        placeholder={this.props.placeholder}
+        onChange={this.props.handleInput}
+        name={this.props.name}
+        type={this.props.type}
+        minLength={this.props.minLength}
+        value={this.props.value}
+      />
+    );
   }
 }
 
-export default UserInput
+export default UserInput;
--- src/components/User/NovoUsuario/NovoUsuario.js
+++ src/components/User/NovoUsuario/NovoUsuario.js
@@ -1,52 +1,62 @@
-import React, { Component } from 'react'
-import UserInput from '../../UI/UserInput/UserInput'
-import './NovoUsuario.scss'
-import PubSub from 'pubsub-js'
-import { Link } from 'react-router-dom'
-import InputMask from 'inputmask'
+import React, { Component } from "react";
+import UserInput from "../../UI/UserInput/UserInput";
+import "./NovoUsuario.scss";
+import PubSub from "pubsub-js";
+import { Link } from "react-router-dom";
+import InputMask from "inputmask";
 
 class NovoUsuario extends Component {
-
   state = {
-    name: '',
-    email:'',
-    cpf:'',
-    tel:'',
-    status:'',
+    name: "",
+    email: "",
+    cpf: "",
+    tel: "",
+    status: ""
+  };
+
+  componentDidMount() {
+    PubSub.subscribe('clear-form', (topico, data) => {this.setState({name:"", email:"", cpf:"", tel:"", status:""})})
   }
 
-  handleInput = (e) => {
-    if(e.target.name === "cpf") {
-      this.setState({[e.target.name]: this.cpfMask.mask(document.getElementsByName("cpf")) })  
+  cpfMask = new InputMask("999.999.999-99");
+
+  handleInput = e => {
+    if (e.target.name === "cpf") {
+      this.setState({
+        [e.target.name]: this.cpfMask.mask(document.getElementsByName("cpf"))
+      });
     }
-    this.setState({[e.target.name]: e.target.value})
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  cpfMask = new InputMask("999.999.999-99")
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.hasEmptyInput(this.state)) {
+      console.log("Tem input vazio");
+    } else {
+      const user = this.createUser();
+      this.addToLocalStorage(user);
+      this.clearForm()
+      PubSub.publish("novo-usuario", user);
+    }
+  };
 
-  // console.log(cpfMask.mask("40174269803"))
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    // this.cpfMask.mask(document.getElementsByName("cpf"))
-    const user = this.createUser()
-    this.addToLocalStorage(user)
-    // window.location.href= "/" // vai para página index
-    PubSub.publish('novo-usuario', user)
-  }
+  clearForm = () => {
+    this.setState({name:"", email:"", cpf:"", tel:"", status:""})
+  } 
 
   addToLocalStorage = user => {
     let users;
 
     if (JSON.parse(localStorage.getItem("users")) == null) {
-      users = []
+      users = [];
     } else {
-      users = JSON.parse(localStorage.getItem("users"))
+      users = JSON.parse(localStorage.getItem("users"));
     }
-    
-    users = users.concat(user)
-    localStorage.setItem("users", JSON.stringify(users))
-  }
+
+    users = users.concat(user);
+    localStorage.setItem("users", JSON.stringify(users));
+  };
 
   createUser = () => {
     const user = {
@@ -58,34 +68,84 @@ class NovoUsuario extends Component {
         tel: this.state.tel
       },
       status: this.state.status
-    }
+    };
 
-    return user
-  }
+    return user;
+  };
+
+  hasEmptyInput = inputs => {
+    let empty = false;
+
+    Object.values(inputs).map(input => {
+      if (input === "") {
+        empty = true;
+      }
+    });
+    return empty;
+  };
 
   render() {
     return (
-        <section className="novo-usuario">
-          <div className="novo-usuario__header">
-            <p>Novo Usuário</p>
-            <p>Informe os campos a seguir para criar novo usuário:</p>
+      <section className="novo-usuario">
+        <div className="novo-usuario__header">
+          <p>Novo Usuário</p>
+          <p>Informe os campos a seguir para criar novo usuário:</p>
+        </div>
+        <form className="novo-usuario__form" onSubmit={this.handleSubmit}>
+          <UserInput
+            id="name"
+            name="name"
+            placeholder="Nome"
+            value={this.state.name}
+            handleInput={this.handleInput}
+            type="text"
+            minLength="3"
+          />
+          <UserInput
+            id="email"
+            name="email"
+            placeholder="E-mail"
+            value={this.state.email}
+            handleInput={this.handleInput}
+            type="email"
+          />
+          <UserInput
+            id="cpf"
+            name="cpf"
+            placeholder="CPF"
+            value={this.state.cpf}
+            handleInput={this.handleInput}
+            type="text"
+            minLength="11"
+          />
+          <UserInput
+            id="telefone"
+            name="tel"
+            placeholder="Telefone"
+            value={this.state.tel}
+            handleInput={this.handleInput}
+            type="text"
+          />
+          <UserInput
+            id="status"
+            name="status"
+            placeholder="Status"
+            value={this.state.status}
+            handleInput={this.handleInput}
+            type="select"
+          />
+          <div className="buttons">
+            <button type="submit" className="create">
+              Criar
+            </button>
+            <Link to="/">
+              <button className="back">Voltar</button>
+            </Link>
           </div>
-          <form className="novo-usuario__form" onSubmit={this.handleSubmit}>
-            <UserInput id="name" name="name" placeholder="Nome" value={this.state.name} handleInput={this.handleInput} type="text" minLength="3"/>
-            <UserInput id="email" name="email" placeholder="E-mail" value={this.state.email} handleInput={this.handleInput} type="email"/>
-            <UserInput id="cpf" name="cpf" placeholder="CPF" value={this.state.cpf} handleInput={this.handleInput} type="text" minLength="11"/>
-            <UserInput id="telefone" name="telefone" placeholder="Telefone" value={this.state.telefone} handleInput={this.handleInput} type="text"/>
-            <UserInput id="status" name="status"  placeholder="Status" value={this.state.status} handleInput={this.handleInput} type="select"/>
-            <div className="buttons">
-              <button type="submit" className="create">Criar</button>
-              <Link to="/">
-                <button className="back">Voltar</button>
-              </Link>
-            </div>
-          </form>
-        </section>
-    )
+        </form>
+      </section>
+    );
   }
 }
 
-export default NovoUsuario
+export default NovoUsuario;
